5ecd7901690710017068b45c434b6e4e4cefe22c
Alicia Urdiales <aliurdialess@gmail.com>
2023-11-15 10:46:55 +0000

    -

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 // const User = require("../models/User.model");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
-const User = require("../models/User.model");
+const User = require("../models/user.model");
 
 //GET "user/login"
 router.get("/login", async (req, res, next) => {
